13f871acbb177a53d1f81bed489f4dd5d6839f1a
saikumard <dsaikumar357@gmail.com>
2021-06-23 20:39:01 +0530

    init commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const App = () => {
+  return(
+    <>
+    <h1>Welcome to React Parcel Micro App!</h1>
+    <p>Hard to get more minimal.</p>
+    <small>Hello this is sai</small>
+    </>
+  );
+  };    
+
+export default App;
+
